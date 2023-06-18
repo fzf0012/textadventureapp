a73afeed3f15797a0da723e2b4be30e9f7b22dbf
Frank <fzf0012@auburn.edu>
2023-06-18 14:22:45 -0400

    Initial Commit

---
+++ TextAdventureApp1/TextAdventureApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  TextAdventureApp1App.swift
+//  TextAdventureApp1
+//
+//  Created by Frank Fukuchi on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TextAdventureApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
